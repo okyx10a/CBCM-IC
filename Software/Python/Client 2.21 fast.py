1d2305c1fda5c2b490fe70d2bee5e13a2b7ae532
Qiao <qiaokuanghua@gmail.com>
2019-01-17 16:01:41 -0500

    Initail commit on github
    Read out software is able to take 1000+ samples per calibration point

---
+++ Software/Python/Client 2.21 fast.py
@@ -0,0 +1,76 @@
+import serial
+import time
+import sys
+import matplotlib.pyplot as plt
+N_sample = 1000
+
+arduino = serial.Serial('COM6',baudrate = 115200, timeout=1)
+print("port opened on " + arduino.name+"\n")
+
+while 1:
+    arduino.reset_input_buffer()
+    arduino.reset_output_buffer()
+    calseq = input("1) \"W#######\" for input calibration sequence\n2) \"G###\" for changing CCO resolution\n3) \"complete test\" for a reading all output for each input ranging from 0-127\n4) \"quit\" to quit program. \nPlease input your command:")
+    if calseq == "quit":
+        arduino.close()
+        sys.exit()
+        
+    elif calseq == "complete test":
+        output = [None]*127*N_sample
+        try:
+            for x in range(127):
+                cmd = "W"+str(format(x,'07b'))
+                arduino.write(cmd.encode())
+                cmd = "R"
+                arduino.write(cmd.encode())
+                time.sleep(0.01)
+                while not arduino.in_waiting:
+                    pass                 
+                temp = arduino.read(size = 2*N_sample)
+                for i in range(N_sample):
+                    output[x*N_sample+i] = int.from_bytes( temp[2*i:2*i+2], byteorder='big')
+                arduino.reset_input_buffer()    
+            plt.plot(output)
+            plt.show()
+            result = list()
+            for i in range(127):
+                result.append(sum(output[i*N_sample:i*N_sample+N_sample])/N_sample)
+            plt.plot(result,'bo')
+            plt.show()
+            f = open("CT_records.csv","w+")
+            for item in output:
+                f.write("%s\n" % item)
+            f.close()
+            f = open("CT_results.csv","w+")
+            for item in result:
+                f.write("%s\n" % item)
+            f.close()
+        except KeyboardInterrupt:
+            time.sleep(1)
+            plot(output)
+            pass          
+    elif calseq[0] == "G" :
+        arduino.write(calseq[1:3].encode())
+        
+    elif calseq[0] == "W" :
+        arduino.write(calseq.encode())
+        output = [0]*N_sample
+        for i in range(N_sample):
+            cmd = "R"
+            arduino.write(cmd.encode())
+            time.sleep(0.01)
+            while not arduino.in_waiting:
+                pass                 
+            temp = arduino.read(size = 2*N_sample)
+            for i in range(N_sample):
+                output[i] = int.from_bytes( temp[2*i:2*i+2], byteorder='big')
+            arduino.reset_input_buffer()  
+        plt.plot(output)
+        plt.show()      
+        f = open("ST_records.csv","w+")
+        for item in output:
+            f.write("%s\n" % item)
+        f.close()
+    
+    
+    
